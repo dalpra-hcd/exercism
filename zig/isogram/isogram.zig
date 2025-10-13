d2fec19feb8411da3e947be4c81b413a211b369e
Hudson C. Dalpra <dalpra.hcd@gmail.com>
2025-10-13 19:10:55 +1300

    solved: zig/isogram no std

--- zig/isogram/isogram.zig
+++ zig/isogram/isogram.zig
@@ -1,18 +1,15 @@
-const std = @import("std");
-
 pub fn isIsogram(str: []const u8) bool {
     var seen: u26 = 0;
 
     for (str) |c| {
-        if (std.ascii.isAlphabetic(c)) {
-            const lower = std.ascii.toLower(c);
-            const bit = @as(u26, 1) << @intCast(lower - 'a');
-
-            if ((seen & bit) != 0) {
-                return false;
-            }
-            seen |= bit;
-        }
+        const bit: u8 = switch (c) {
+            'A'...'Z' => c - 'A',
+            'a'...'z' => c - 'a',
+            else => continue,
+        };
+        const mask = @as(u26, 1) << @intCast(bit);
+        if ((seen & mask) != 0) return false;
+        seen |= mask;
     }
 
     return true;
