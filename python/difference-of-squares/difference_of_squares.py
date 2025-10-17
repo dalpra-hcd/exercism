0df377e6b964c4046e6d030322302a663d2602b9
Hudson C. Dalpra <dalpra.hcd@gmail.com>
2025-10-17 21:44:12 +1300

    solved: python/difference-of-squares

---
+++ python/difference-of-squares/difference_of_squares.py
@@ -0,0 +1,11 @@
+def square_of_sum(n):
+    sum = (n * (n + 1)) / 2
+    return sum * sum
+
+
+def sum_of_squares(n):
+    return (n * (n + 1) * (2 * n + 1)) / 6
+
+
+def difference_of_squares(n):
+    return square_of_sum(n) - sum_of_squares(n)
